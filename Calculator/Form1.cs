eed0b62193fea04e66a8075624ba3884a197f53a
KeerthiSenpai <nkeerthi491999@gmail.com>
2023-10-31 20:32:17 +0530

    Add project files.

---
+++ Calculator/Form1.cs
@@ -0,0 +1,35 @@
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
